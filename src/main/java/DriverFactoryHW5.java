f1430718b074daef8893092dd3aa95e1c9945291
Vladislav Klochkov <v.i.klo4kov@yandex.ru>
2024-03-12 23:18:37 +0400

    added HW5

---
+++ src/main/java/DriverFactoryHW5.java
@@ -0,0 +1,26 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_ACTIVITY;
+import static io.appium.java_client.remote.AndroidMobileCapabilityType.APP_PACKAGE;
+import static io.appium.java_client.remote.MobileCapabilityType.NO_RESET;
+
+public class DriverFactoryHW5 {
+    AndroidDriver<?> driver;
+
+    public AndroidDriver<?> setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "ANDROID");
+        capabilities.setCapability(APP_PACKAGE, "ru.filit.mvideo.b2c");
+        capabilities.setCapability(APP_ACTIVITY, "ui.splash.view.SplashScreen");
+        capabilities.setCapability(NO_RESET, true);
+        URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+        driver = new AndroidDriver<>(remoteUrl, capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+}
